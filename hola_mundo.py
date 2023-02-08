5d222e658c22046d3e5fe8ddba67b32fa48c51e8
TaniaBalam <107269699+TaniaBalam@users.noreply.github.com>
2023-02-08 15:57:50 -0600

    quite mensajes innecesarios

--- hola_mundo.py
+++ hola_mundo.py
@@ -13,9 +13,4 @@ print(exam_grade(60)) # Should be Pass
 print(exam_grade(95)) # Should be Pass
 print(exam_grade(100)) # Should be Top Score
 print(exam_grade(0)) # Should be Fail
-
-print("Hola mundo")
-print("Hola Tania")
-print("Hello Misael")
-
 print(5-2)
